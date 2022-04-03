bc00369d90d6be53cc0dec8bbfc7c466d0db001d
lomesh <badhelomesh82@gmail.com>
2022-04-03 20:26:16 +0530

    Fix: Page not found error

--- src/frontend/components/Routelist.jsx
+++ src/frontend/components/Routelist.jsx
@@ -14,6 +14,7 @@ import {
   Explore,
   SingleVideo,
 } from "frontend/pages";
+import { Pathnotfound } from "frontend/components";
 
 const Routelist = () => {
   return (
@@ -21,20 +22,21 @@ const Routelist = () => {
       <Routes>
         <Route path="/" exact element={<Homepage />} />
         <Route path="/login" element={<Login />} />
-        <Route path="/signup" exact element={<Signup />} />
-        <Route path="/profile" exact element={<Profile />} />
-        <Route path="/videos/:videoId" exact element={<SingleVideo />} />
-        <Route path="/videos" exact element={<VideoGrid />}>
-          <Route path="explore" exact element={<Explore />} />
-          <Route path="playlist" exact element={<Playlist />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/videos/:videoId" element={<SingleVideo />} />
+        <Route path="*" element={<Pathnotfound />} />
+        <Route path="/videos" element={<VideoGrid />}>
+          <Route path="explore" element={<Explore />} />
+          <Route path="playlist" element={<Playlist />} />
           <Route
             path="playlist/:playlistId"
             exact
             element={<SinglePlaylist />}
           />
-          <Route path="liked" exact element={<LikedVideo />} />
-          <Route path="watchlater" exact element={<WatchLater />} />
-          <Route path="history" exact element={<History />} />
+          <Route path="liked" element={<LikedVideo />} />
+          <Route path="watchlater" element={<WatchLater />} />
+          <Route path="history" element={<History />} />
         </Route>
       </Routes>
     </div>
--- src/frontend/components/index.jsx
+++ src/frontend/components/index.jsx
@@ -3,3 +3,4 @@ export { Nav } from "./Nav";
 export { Routelist } from "./Routelist";
 export { Notify } from "./Toast";
 export { Loader } from "./Loader";
+export { Pathnotfound } from "./Pagenotfound";
